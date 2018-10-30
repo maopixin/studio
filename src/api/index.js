6fb78571ff99efa8cb7c0583b2e04875bbe764de
maopixin <18364076997@163.com>
2018-10-30 17:31:54 +0800

    ss

--- src/api/index.js
+++ src/api/index.js
@@ -8,7 +8,8 @@ axios.interceptors.request.use(
     err => {
         return Promise.reject(err);
     });
-// http response 服务器响应拦截器，这里拦截401错误，并重新跳入登页重新获取token 
+
+//  http response 服务器响应拦截器，这里拦截401错误，并重新跳入登页重新获取token 
 // axios.interceptors.response.use(
 //     response => {
 //         return response;
